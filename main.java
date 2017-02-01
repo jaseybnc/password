6a6f28c71c6f3fa1cc101ba532c0cb8b770c8532
Jasey <nhjasey@gmail.com>
2017-02-01 16:02:56 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+
+public class PasswordGenerator 
+{
+    public static void main(String[] args)
+    {
+        int length = 12; // our password length
+        System.out.println(generatePswd(length));
+    }
+    static char[] generatePswd(int len)
+    {
+        System.out.println("Your Password:");
+        String charsCaps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // our capital letters for inclusion
+        String chars = "abcdefghijklmnopqrstuvwxyz";  // lowercase letters
+        String nums = "0123456789";  // numbers
+        String symbols = "!@#$%^&*_=+-/€.?<>)"; // symbols included in our random password
+
+        String passSymbols = charsCaps + chars + nums + symbols;
+        Random rnd = new Random();
+        
+        char[] password = new char[len];
+        int index = 0;
+        for (int i = 0; i < len; i++) 
+        {
+            password[i] = passSymbols.charAt(rnd.nextInt(passSymbols.length()));
+            
+        }
+        return password;
+    }
+}
